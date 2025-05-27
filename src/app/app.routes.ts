f5f2a3f9ff4ac5dcbc199e4ae47ef59b19d08b85
sqw1sh <zebraalaalaa@gmail.com>
2025-05-27 18:10:57 +0300

    add components structure, base styles, routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/other/main/main.component';
+import { PageNotFoundComponent } from './components/other/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
